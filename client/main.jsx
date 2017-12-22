439125fc9b11654641e6434e5d607e92b1a948bf
pomelyu <pomelyu5199@icloud.com>
2017-12-22 21:31:21 +0800

    add react package

---
+++ client/main.jsx
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from '../imports/ui/components/App';
+
+Meteor.startup(() => {
+  render(
+    <App />,
+    document.getElementById('app'));
+});
